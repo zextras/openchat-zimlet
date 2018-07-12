59126f7fec2a00332949dd31ad666ba465c35a58
Ugo Padoan <up@zextras.com>
2018-07-12 11:00:45 +0200

    Used undefined instead of delete to unset a variable in ReactDwtComposite

--- src/dwt/widgets/ReactDwtComposite.tsx
+++ src/dwt/widgets/ReactDwtComposite.tsx
@@ -69,7 +69,7 @@ export abstract class ReactDwtComposite<P, S extends React.ComponentState>
 
   public unmountComponent(): void {
     ReactDOM.unmountComponentAtNode(ReactDOM.findDOMNode(this.getHtmlElement()));
-    delete this.mComponent;
+    this.mComponent = undefined;
   }
 
   protected setState<K extends keyof S>(state: Pick<S, K>): void {
