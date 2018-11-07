3329a9c0e51bf5a30fb832cc73857bbb2c41c857
Lisa Parma <lisa.parma@zextras.com>
2018-11-07 11:06:19 +0100

    Deleted alias for preact-compat

--- config/webpack.config.app.dev.js
+++ config/webpack.config.app.dev.js
@@ -99,9 +99,7 @@ module.exports = {
 
       // Support React Native Web
       // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
-      'react-native': 'react-native-web',
-      'react': 'preact-compat',
-      'react-dom': 'preact-compat',
+      'react-native': 'react-native-web'
     },
     plugins: [
       // Prevents users from importing files from outside of src/ (or node_modules/).
